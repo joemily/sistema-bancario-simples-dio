08633e1e7bb54838ea528aaeb1816b30f8236c94
joemilybc <joemilybcampos@gmail.com>
2023-08-31 15:09:19 -0300

    Inserção de outra opção

--- projeto.py
+++ projeto.py
@@ -5,6 +5,7 @@ ______________________
 |[1] Depositar       |
 |[2] Sacar           |
 |[3] Extrato         |
+|[4] Ajuda           |
 |[0] Sair            |
 |____________________|
 
@@ -22,35 +23,34 @@ while True:
     opcao = int(input(menu))
 
     if opcao == 1:
-        valor = float(input("\nInforme o valor do depósito: "))
+        valor = float(input("\n|Informe o valor do depósito: "))
 
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
         
         else:
-            print("Falha na operação! Valor inválido.")
+            print("|Falha na operação! Valor inválido.|")
 
     elif opcao == 2:
-        valor = float(input("Informe o valor do saque: "))
+        valor = float(input("|Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > limite
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print('\nFalha na operação! Saldo insuficiente.')
+            print('\n|Falha na operação! Saldo insuficiente.|')
         elif excedeu_limite:
-            print('\nFalha na operação! Limite de saque excedido.')
+            print('\n|Falha na operação! Limite de saque excedido.|')
         elif excedeu_saques:
-            print('\nFalha na operação! Número máximo de saques excedido.')
+            print('\n|Falha na operação! Número máximo de saques excedido.|')
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
-            print(type(extrato))
             numero_saques += 1
         else:
-            print('Falha na operação! O valor informado é inválido.')
+            print('|Falha na operação! O valor informado é inválido|')
     
     elif opcao == 3:
         print('\n================ EXTRATO ================')
@@ -59,9 +59,17 @@ while True:
         print(f'\nSaldo: R$ {saldo:.2f}') 
         print('=========================================')
     
+    elif opcao == 4:
+        print('================================================')
+        print('Informações')
+        print('1. Valor máximo do saque: R$ 500.00')
+        print('2. Quantidade máxima de saques: 3 saques por dia')
+        print('=> Para mais informações contate o seu gerente')
+        print('================================================')
+
     elif opcao == 0:
         break
     
     else:
-        print('Operação inválida, por favor selecione novamente a operação desejada.')
+        print('|Operação inválida, por favor selecione novamente a operação desejada.|')
 
